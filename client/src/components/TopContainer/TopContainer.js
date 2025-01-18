a9bf1ac5d4335281a7d9f9bb6ee2f2d20f1e42ee
chandini <chandinisureshkumar@gmail.com>
2025-01-18 13:00:35 +0530

    photo

--- client/src/components/TopContainer/TopContainer.js
+++ client/src/components/TopContainer/TopContainer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './TopContainer.css';
 import TopContent from '../TopContent/TopContent';
+import photo from './photo.webp';
 import { Element } from 'react-scroll';
 
 const TopContainer = () => {
     return (
-        <Element name="about" className='topContainer'>
+        <Element name="about" className='topContainer'style={ {backgroundImage: `url(${photo})`}}>
             <TopContent />
         </Element>
     );
