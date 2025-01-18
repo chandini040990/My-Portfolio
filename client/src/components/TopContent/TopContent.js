48c6402c209a49e55a4142db55688732438ecc52
chandini <chandinisureshkumar@gmail.com>
2025-01-18 13:16:13 +0530

    pdf

--- client/src/components/TopContent/TopContent.js
+++ client/src/components/TopContent/TopContent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-scroll';
 import './TopContent.css';
+import Chandini_Resume_Latest from '../TopContent/Chandini_Resume_Latest.pdf';
 
 const TopContent = () => {
     return (
@@ -8,7 +9,7 @@ const TopContent = () => {
             <div className='topContent_container'>
                 <h1>Chandini Sureshkumar</h1>
                 <p>Full stack Developer - MERN</p>
-                <a href="www.google.com">
+                <a href={Chandini_Resume_Latest} download="DownloadCV" target='_blank'>
                     <button className='topContent_downloadButton'>Download CV</button>
                 </a>
                 <Link to="projects" smooth={true} duration={500}>
