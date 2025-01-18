0f91fe7f9c6a9a7f033dc7859631df591d22559b
chandini <chandinisureshkumar@gmail.com>
2025-01-18 12:55:05 +0530

    exp

--- client/src/components/Experience/Experience.js
+++ client/src/components/Experience/Experience.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Experience.css';
 import ExperienceBox from '../ExperienceBox/ExperienceBox';
+import exp from './exp.jpeg';
 import { Element } from 'react-scroll';
 
 const Experience = () => {
     return (
         <Element name="exp" className='expContainer'>
             <h1>Experience</h1>
-            <div className='expContainer_info'>
+            <div className='expContainer_info' style={ {backgroundImage: `url(${exp})`}}>
                 <ExperienceBox number="+5" title="Years" />
                 <ExperienceBox number="+20" title="Projects" style={{ backgroundColor: "#f64c08" }} />
                 <ExperienceBox number="+10000" title="Lines of code" />
